0633be117ab62d68ad3197f162375276d8060167
danielbragadasilva <danyelsgs@gmail.com>
2025-02-21 16:20:51 -0300

    🚀 :rocket:	project completion

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,12 +1,23 @@
 import Image from 'next/image'
+import logo from '../../../assets/logo.svg'
 
-import logo from '../../assets/logo.svg'
 import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 
-export default function InvitePage() {
-  const inviteLink = 'htpp://localhost:3000/invite/dmaksmdksmk1k233m132'
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+//20:41
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -33,7 +44,7 @@ export default function InvitePage() {
           </div>
           <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
       <Ranking />
