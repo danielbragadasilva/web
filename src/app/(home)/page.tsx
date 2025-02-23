ba0f24e7201d1f4166e5372e383a91458023321b
danielbragadasilva <danyelsgs@gmail.com>
2025-02-23 17:06:48 -0300

    update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Radio } from 'lucide-react'
 import logo from '../../assets/logo.svg'
 import { SubscriptionForm } from './subscription-form'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
@@ -37,7 +38,9 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito{' '}
           </p>
         </div>
-        <SubscriptionForm />
+        <Suspense fallback={<div>Carregando...</div>}>
+      <SubscriptionForm />
+    </Suspense>
       </div>
     </div>
   )
